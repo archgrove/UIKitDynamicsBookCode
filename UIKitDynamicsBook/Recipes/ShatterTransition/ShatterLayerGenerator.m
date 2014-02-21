1f7f49c66df8dfcf47aac5eaa22cbf59427fb03b
Adam Wright <adamw@archgrove.co.uk>
2014-02-21 14:38:09 +0000

    Better geometry calculations for shatter frames. Lots of cleanup to do!

--- UIKitDynamicsBook/Recipes/ShatterTransition/ShatterLayerGenerator.m
+++ UIKitDynamicsBook/Recipes/ShatterTransition/ShatterLayerGenerator.m
@@ -8,6 +8,124 @@
 
 #import "ShatterLayerGenerator.h"
 
+BOOL linesIntersect(CGPoint l1Start, CGPoint l1End, CGPoint l2Start, CGPoint l2End, CGPoint *result)
+{
+    // Cross product approach to line-segment intersections
+    // See http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+    const CGFloat EPSILON = 0.0001;
+    CGFloat d = (l1End.x - l1Start.x) * (l2End.y - l2Start.y) - (l1End.y - l1Start.y) * (l2End.x - l2Start.x);
+    
+    if (fabs(d) < EPSILON)
+        return NO;
+    
+    CGFloat u = ((l2Start.x - l1Start.x) * (l2End.y - l2Start.y) - (l2Start.y - l1Start.y) * (l2End.x - l2Start.x)) / d;
+    CGFloat v = ((l2Start.x - l1Start.x) * (l1End.y - l1Start.y) - (l2Start.y - l1Start.y) * (l1End.x - l1Start.x)) / d;
+    
+    if (u < 0.0 || u > 1.0)
+        return NO;
+    
+    if (v < 0.0 || v > 1.0)
+        return NO;
+    
+    CGPoint intersection;
+    intersection.x = l1Start.x + u * (l1End.x - l1Start.x);
+    intersection.y = l1Start.y + u * (l1End.y - l1Start.y);
+    
+    if (fabs(intersection.x) < EPSILON)
+        intersection.x = 0;
+    if (fabs(intersection.y) < EPSILON)
+        intersection.y = 0;
+    
+    *result = intersection;
+    
+    return YES;
+}
+
+CGPoint targetPointOrNextCorner(CGRect rect, CGPoint current, CGPoint target)
+{
+    // Assumes that current and target are on the rect
+    CGPoint connectTo = CGPointZero;
+    
+    if (current.y == 0 && current.x < rect.size.width)
+    {
+        // Current point is on the top line
+        if (target.y == 0 && target.x >= current.x)
+            return target;
+
+        connectTo = CGPointMake(rect.size.width, 0);
+    }
+    if (current.x == rect.size.width && current.y < rect.size.height)
+    {
+        // Current point is on the right line
+        if (target.x == rect.size.width && target.y >= current.y)
+            return target;
+        
+        connectTo = CGPointMake(rect.size.width, rect.size.height);
+    }
+    if (current.y == rect.size.height && current.x > 0)
+    {
+        // Bottom line
+        if (target.y == rect.size.height && target.x <= current.x)
+            return target;
+        
+        connectTo = CGPointMake(0, rect.size.height);
+    }
+    else if (current.x == 0 && current.y > 0)
+    {
+        // Left line
+        if (target.x == 0 && target.y <= current.y)
+            return target;
+        
+        connectTo = CGPointMake(0, 0);
+    }
+
+    return connectTo;
+}
+
+void addPerimeterConnectionBetweenPoints(CGRect rect, CGMutablePathRef path, CGPoint p1, CGPoint p2)
+{
+    // Start at p1
+    CGPathAddLineToPoint(path, NULL, p1.x, p1.y);
+    NSLog(@"Point %f %f", p1.x, p1.y);
+    
+    // Clockwise walk the four possible sides to connect the two points
+    CGPoint p = targetPointOrNextCorner(rect, p1, p2);
+    while (p.x != p2.x || p.y != p2.y)
+    {
+        CGPathAddLineToPoint(path, NULL, p.x, p.y);
+        p = targetPointOrNextCorner(rect, p, p2);
+    }
+    
+    // End at p2
+    CGPathAddLineToPoint(path, NULL, p2.x, p2.y);
+    NSLog(@"Point %f %f", p2.x, p2.y);
+}
+
+CGPoint lineRectIntersection(CGRect rect, CGPoint lineStart, CGPoint lineEnd)
+{
+    // Consider each rect segment separately
+    CGPoint segments[] = {
+        // Top line
+        CGPointMake(0, 0), CGPointMake(rect.size.width, 0),
+        // Right line
+        CGPointMake(rect.size.width, 0), CGPointMake(rect.size.width, rect.size.height),
+        // Bottom line
+        CGPointMake(rect.size.width, rect.size.height), CGPointMake(0, rect.size.height),
+        // Left line
+        CGPointMake(0, rect.size.height), CGPointMake(0, 0)
+    };
+    
+    CGPoint intersection = CGPointZero;
+    
+    for (int i = 0; i < sizeof(segments) / sizeof(CGPoint); i += 2)
+    {
+        if (linesIntersect(lineStart, lineEnd, segments[i], segments[i + 1], &intersection))
+            break;
+    }
+    
+    return intersection;
+}
+
 @implementation ShatterLayerGenerator
 
 - (instancetype)initWithBounds:(CGRect)bounds
@@ -19,8 +137,8 @@
         self.bounds = bounds;
         
         self.rings = 5;
-        self.ringSpacing = bounds.size.width / self.rings;
-        self.points = 7;
+        self.ringSpacing = bounds.size.width / (2 * self.rings);
+        self.points = 12;
         self.jitter = 0.05;
     }
     
@@ -50,12 +168,14 @@
             
             [targetPoints addObject:[NSValue valueWithCGPoint:CGPointMake(p.x + sin(angle) * radius, p.y + cos(angle) * radius)]];
         }
-        
-        // Now we need to connect it to the edge; instead, we connect it far off the edge and force CG to do our clipping for us
-        // This is more resource intensive, but results in much easier code
+
         CGFloat angle = [a floatValue];
-        [targetPoints addObject:[NSValue valueWithCGPoint:CGPointMake(p.x + sin(angle) * 2 * self.bounds.size.height,
-                                                                      p.y + cos(angle) * 2 * self.bounds.size.height)]];
+        CGPoint farOutPoint = CGPointMake(p.x + sin(angle) * 5 * self.bounds.size.height,
+                                         p.y + cos(angle) * 5 * self.bounds.size.height);
+        
+        CGPoint intersectionPoint = lineRectIntersection(self.bounds, p, farOutPoint);
+        NSLog(@"Intersection point %f %f", intersectionPoint.x, intersectionPoint.y);
+        [targetPoints addObject:[NSValue valueWithCGPoint:intersectionPoint]];
         
         [pointSets addObject:targetPoints];
 
@@ -73,17 +193,23 @@
         CGMutablePathRef path = CGPathCreateMutable();
         
         CGPoint startPoint = [pointsOut[0] CGPointValue];
+        [self logPoint:startPoint];
         
         CGPathMoveToPoint(path, NULL, startPoint.x, startPoint.y);
         for (int j = 1; j < pointsOut.count; j++)
         {
             CGPoint lP = [pointsOut[j] CGPointValue];
+                    [self logPoint:lP];
             CGPathAddLineToPoint(path, NULL, lP.x, lP.y);
         }
         
+        addPerimeterConnectionBetweenPoints(self.bounds, path, [pointsOut.lastObject CGPointValue], [pointsIn.lastObject CGPointValue]);
+        
         for (NSValue *v in [pointsIn reverseObjectEnumerator])
         {
+            
             CGPoint lP = v.CGPointValue;
+                    [self logPoint:lP];
             CGPathAddLineToPoint(path, NULL, lP.x, lP.y);
         }
         
@@ -91,8 +217,8 @@
         CAShapeLayer *shapeLayer = [CAShapeLayer layer];
         shapeLayer.bounds = self.bounds;
         shapeLayer.fillColor = [UIColor blackColor].CGColor;
-        shapeLayer.strokeColor = [UIColor clearColor].CGColor;
-        shapeLayer.lineWidth = 0;
+        shapeLayer.strokeColor = [UIColor redColor].CGColor;
+        shapeLayer.lineWidth = 2;
         shapeLayer.path = path;
         shapeLayer.anchorPoint = CGPointMake(0, 0);
         CGPathRelease(path);
@@ -101,6 +227,11 @@
     }
 
     return shapeLayers;
+}
+
+- (void)logPoint:(CGPoint)p
+{
+    NSLog(@"Point %f %f", p.x, p.y);
 }
 
 - (NSArray*)generateRandomNumbersInRange:(CGFloat)start to:(CGFloat)end partitions:(NSUInteger)count
@@ -117,7 +248,7 @@
     }
     
     return [randomFactors sortedArrayWithOptions:NSSortStable usingComparator:^NSComparisonResult(id obj1, id obj2) {
-        return [obj1 floatValue] > [obj2 floatValue];
+        return [obj1 floatValue] < [obj2 floatValue];
     }];
 }
 
--- UIKitDynamicsBook/Recipes/ShatterTransition/ShatterViewController.m
+++ UIKitDynamicsBook/Recipes/ShatterTransition/ShatterViewController.m
@@ -8,11 +8,14 @@
 
 #import "ShatterViewController.h"
 
+#import "ShatterLayerGenerator.h"
+
 #import "ViewShatterer.h"
 
 @implementation ShatterViewController
 {
     UIView *containerView;
+    UIView *testView;
     
     ViewShatterer *shatterer;
 }
@@ -26,13 +29,29 @@
 
     containerView = nibViews[0];
     containerView.center = self.view.center;
+
     
     [self.view addSubview:containerView];
+
+    
     
     shatterer = [[ViewShatterer alloc] initWithView:containerView containerView:self.view];
     
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
     [containerView addGestureRecognizer:tapRecognizer];
+    
+    
+    
+    /*
+    testView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+    testView.center = self.view.center;
+    [self.view addSubview:testView];
+    
+    ShatterLayerGenerator *gen = [[ShatterLayerGenerator alloc] initWithBounds:testView.bounds];
+    NSArray *layers = [gen generateShatterLayersFromPoint:CGPointMake(150, 150)];
+    
+    for (CALayer *layer in layers)
+        [testView.layer addSublayer:layer];*/
 }
 
 - (void)viewTapped:(UITapGestureRecognizer*)tapRecognizer
@@ -41,5 +60,11 @@
     [containerView removeFromSuperview];
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"View shattering";
+}
 
 @end
